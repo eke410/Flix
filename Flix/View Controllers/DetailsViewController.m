54bf2f5d1e97c66c1e90f84dd484aa35ae94a9f8
eke410 <elizabethke100@gmail.com>
2021-06-24 19:35:35 -0400

    in DetailsViewController, updated backdropView display by first loading low-resolution image followed by a high-resolution image

--- Flix/View Controllers/DetailsViewController.m
+++ Flix/View Controllers/DetailsViewController.m
@@ -23,17 +23,56 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    // loads posterView
     NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
     NSString *posterURLString = self.movie[@"poster_path"];
     NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
     NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
     [self.posterView setImageWithURL:posterURL];
     
+    // loads backdropView - first loads low-resolution image, then followed by a high-resolution image
+    NSString *baseURLStringLowRes = @"https://image.tmdb.org/t/p/w500";
+    NSString *baseURLStringHighRes = @"https://image.tmdb.org/t/p/original";
     NSString *backdropURLString = self.movie[@"backdrop_path"];
-    NSString *fullBackdropURLString = [baseURLString stringByAppendingString:backdropURLString];
-    NSURL *backdropURL = [NSURL URLWithString:fullBackdropURLString];
-    [self.backdropView setImageWithURL:backdropURL];
     
+    NSString *fullBackdropURLStringLowRes = [baseURLStringLowRes stringByAppendingString:backdropURLString];
+    NSString *fullBackdropURLStringHighRes = [baseURLStringHighRes stringByAppendingString:backdropURLString];
+
+    NSURL *backdropURLLowRes = [NSURL URLWithString:fullBackdropURLStringLowRes];
+    NSURL *backdropURLHighRes = [NSURL URLWithString:fullBackdropURLStringHighRes];
+
+    NSURLRequest *requestSmall = [NSURLRequest requestWithURL:backdropURLLowRes];
+    NSURLRequest *requestLarge = [NSURLRequest requestWithURL:backdropURLHighRes];
+
+    __weak DetailsViewController *weakSelf = self;
+    [self.backdropView setImageWithURLRequest:requestSmall placeholderImage:nil success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *smallImage) {
+        // smallImageResponse will be nil if the smallImage is already available
+        // in cache (might want to do something smarter in that case).
+        weakSelf.backdropView.alpha = 0.0;
+        weakSelf.backdropView.image = smallImage;
+
+        [UIView animateWithDuration:0.3 animations:^{
+            weakSelf.backdropView.alpha = 1.0;
+        } completion:^(BOOL finished) {
+            // The AFNetworking ImageView Category only allows one request to be sent at a time
+            // per ImageView. This code must be in the completion block.
+            [weakSelf.backdropView setImageWithURLRequest:requestLarge placeholderImage:smallImage success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage * largeImage) {
+                weakSelf.backdropView.image = largeImage;
+            }
+            failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
+               // do something for the failure condition of the large image request
+               // possibly setting the ImageView's image to a default image
+            }];
+        }];
+    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
+        // do something for the failure condition
+        // possibly try to get the large image
+        NSString *fullBackdropURLString = [baseURLString stringByAppendingString:backdropURLString];
+        NSURL *backdropURL = [NSURL URLWithString:fullBackdropURLString];
+        [self.backdropView setImageWithURL:backdropURL];
+    }];
+    
+    // sets title and synopsis
     self.titleLabel.text = self.movie[@"title"];
     self.synopsisLabel.text = self.movie[@"overview"];
     
