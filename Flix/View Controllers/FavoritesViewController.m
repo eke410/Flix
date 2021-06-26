9cc1a77249f85a3f1a34b4fbf55a6010e3b41a35
eke410 <elizabethke100@gmail.com>
2021-06-25 22:22:24 -0400

    implemented basic favorites feature

--- Flix/View Controllers/FavoritesViewController.m
+++ Flix/View Controllers/FavoritesViewController.m
@@ -7,12 +7,14 @@
 
 #import "FavoritesViewController.h"
 #import "MovieCell.h"
+#import "DetailsViewController.h"
 #import "UIImageView+AFNetworking.h"
 
 @interface FavoritesViewController () <UITableViewDataSource, UITableViewDelegate>
 
-@property (nonatomic, strong) NSArray *movieIDs;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSArray *movieIDs;
+@property (nonatomic, strong) NSArray *movies;
 
 @end
 
@@ -23,31 +25,80 @@
     
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
-    
+}
+
+- (void)fetchFavorites {
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     self.movieIDs = [defaults arrayForKey:@"favoriteIDs"];
     
-    [self.tableView reloadData];
-
+    self.movies = [[NSArray alloc] init];
+    for(NSString *movieID in self.movieIDs) {
+        NSString *requestURL = [[@"https://api.themoviedb.org/3/movie/" stringByAppendingFormat:@"%@", movieID] stringByAppendingFormat:@"?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US"];
+       
+        NSURL *url = [NSURL URLWithString:requestURL];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+        NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            if (error != nil) {
+                NSLog(@"%@", [error localizedDescription]);
+            }
+            else {
+                NSDictionary *movie = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+                self.movies = [self.movies arrayByAddingObject:movie];
+                [self.tableView reloadData];
+            }
+        }];
+        [task resume];
+    }
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 5;
-//    return self.movieIDs.count;
+    return self.movies.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-
-    MovieCell *cell = [[MovieCell alloc] init];
-    cell.titleLabel.text = @"temp title label";
+    NSDictionary *movie = self.movies[indexPath.row];
+    
+    MovieCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MovieCell" forIndexPath:indexPath];
+    NSURL *posterURL = [NSURL URLWithString:[@"https://image.tmdb.org/t/p/w500" stringByAppendingString:movie[@"poster_path"]]];
+    [cell.posterView setImageWithURL:posterURL];
+    cell.titleLabel.text = movie[@"title"];
+    cell.synopsisLabel.text = movie[@"overview"];
+    
     return cell;
+}
+
+- (void) viewWillAppear:(BOOL)animated {
+    NSLog(@"Tab clicked");
+    [self fetchFavorites];
+}
+
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    
+    UITableViewCell *tappedCell = sender;
+    NSIndexPath *indexPath = [self.tableView indexPathForCell:tappedCell];
+    NSDictionary *movie = self.movies[indexPath.row];
+
+    DetailsViewController *detailsViewController = [segue destinationViewController];
+    detailsViewController.movie = movie;
+}
+
+
+@end
+
+
+
     
     
-//    MovieCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MovieCell"];
 //
-//    NSString *movieID = self.movieIDs[indexPath.row];
 //
-//    NSString *requestURL = [[@"https://api.themoviedb.org/3/movie/" stringByAppendingFormat:@"%@", movieID] stringByAppendingFormat:@"?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US"];
+//  NSString *requestURL = [[@"https://api.themoviedb.org/3/movie/" stringByAppendingFormat:@"%@", movieID] stringByAppendingFormat:@"?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US"];
 //
 //    NSURL *url = [NSURL URLWithString:requestURL];
 //    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
@@ -92,17 +143,6 @@
 //           }
 //       }];
 //    return cell;
-}
+//
 
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
-@end
